bd88e26a80ecfd4a4c7c3e4e91271a59b98d1937
Rithvik-Chikatla <98206714+Rithvik-Chikatla@users.noreply.github.com>
2023-10-05 18:33:55 +0530

    Updates

---
+++ precache-manifest.eb5deaa0913e2c8fc0f46b6bf419f4d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249e1235f7b4dc26d812b3ece28f161d",
+    "url": "/React-TextUtils/index.html"
+  },
+  {
+    "revision": "37609f85ba6bbbc2e57a",
+    "url": "/React-TextUtils/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0768a39992954861db71",
+    "url": "/React-TextUtils/static/js/2.fb2bc71b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React-TextUtils/static/js/2.fb2bc71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a072033ecbb367b3e42",
+    "url": "/React-TextUtils/static/js/3.e5658f37.chunk.js"
+  },
+  {
+    "revision": "37609f85ba6bbbc2e57a",
+    "url": "/React-TextUtils/static/js/main.ed820adf.chunk.js"
+  },
+  {
+    "revision": "979963d7fb2c106d6cf9",
+    "url": "/React-TextUtils/static/js/runtime-main.6f6c5457.js"
+  }
+]);
